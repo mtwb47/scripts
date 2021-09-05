7a0464e5f0b086608d6610880389299dc5bc7a69
Matthew Weber <mtwb47@gmail.com>
2021-09-05 15:48:18 -0400

    Start of a new script

---
+++ changer.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Rice Changer 
+# Author: Matt Weber AKA The Linux Cast
+# Updated: 09-01-2021
+# Version: 0.1-9121
+# Desc: This script will change between DWM rices.
+#
+#
+#
+
+
+Remove old sucklesss folder
+copy new rice to suckless folder
+make install
+
+
