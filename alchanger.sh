ed770ec957d2e363ba15702e97fb9cf374f81bb1
Scraft161 <Scraft161@gmail.com>
2021-11-04 21:48:47 +0100

    change if-elif chain to case

--- alchanger.sh
+++ alchanger.sh
@@ -17,26 +17,25 @@ declare -a options=(
 
 choice=$(printf '%s\n' "${options[@]}" | rofi -dmenu -i -l 20 -p 'Themes')
 
-if [[ "$choice" == quit ]]; then
-    echo "No Theme Chosen" && exit 1
-elif [[ "$choice" == 'doom-one' ]]; then
-    sed -i '/colors:/c\colors: *doom-one' $config
-elif [[ "$choice" == 'dracula' ]]; then
-    sed -i '/colors:/c\colors: *dracula' $config
-elif [[ "$choice" == 'gruvbox-dark' ]]; then
-    sed -i '/colors:/c\colors: *gruvbox-dark' $config
-elif [[ "$choice" == 'monokai-pro' ]]; then
-    sed -i '/colors:/c\colors: *monokai-pro' $config
-elif [[ "$choice" == 'nord' ]]; then
-    sed -i '/colors:/c\colors: *nord' $config
-elif [[ "$choice" == 'oceanic-next' ]]; then
-    sed -i '/colors:/c\colors: *oceanic-next' $config
-elif [[ "$choice" == 'solarized-light' ]]; then
-    sed -i '/colors:/c\colors: *solarized-light' $config
-elif [[ "$choice" == 'solarized-dark' ]]; then
-    sed -i '/colors:/c\colors: *solarized-dark' $config
-elif [[ "$choice" == 'tomorrow-night' ]]; then
-    sed -i '/colors:/c\colors: *tomorrow-night' $config
-fi
-
-
+case $choice in
+    'doom-one')
+        sed -i '/colors:/c\colors: *doom-one' $config ;;
+    'dracula')
+        sed -i '/colors:/c\colors: *dracula' $config ;;
+    'gruvbox-dark')
+        sed -i '/colors:/c\colors: *gruvbox-dark' $config ;;
+    'monokai-pro')
+        sed -i '/colors:/c\colors: *monokai-pro' $config ;;
+    'nord')
+        sed -i '/colors:/c\colors: *nord' $config ;;
+    'oceanic-next')
+        sed -i '/colors:/c\colors: *oceanic-next' $config ;;
+    'solarized-light')
+        sed -i '/colors:/c\colors: *solarized-light' $config ;;
+    'solarized-dark')
+        sed -i '/colors:/c\colors: *solarized-dark' $config ;;
+    'tomorrow-night')
+        sed -i '/colors:/c\colors: *tomorrow-night' $config ;;
+    'quit')
+        echo "No theme chosen" && exit 1 ;;
+esac
